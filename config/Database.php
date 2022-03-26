94350305f9f61bda29d5e4999568dada7e348c2c
Curtis Ruder HP <curtisjruder@gmail.com>
2022-03-26 16:32:27 -0500

    Created database schema, data, class

---
+++ config/Database.php
@@ -0,0 +1,44 @@
+<?php
+    class Database{
+        private $conn;
+    
+        private $hostname;
+        private $username;
+        private $password;
+        private $database;
+
+
+        public function __construct(){   
+            ///////////////////////////////////////////////////   
+            ////    CODE for when the database is hosted   ////
+            ///////////////////////////////////////////////////   
+
+            //$url = getenv('JAWSDB_URL');
+            //$dbparts = parse_url($url);
+            //$this->hostname = $dbparts['host'];
+            //$this->username = $dbparts['user'];
+            //$this->password = $dbparts['pass'];
+            //$this->database = ltrim($dbparts['path'],'/');   
+            
+            //////////////////////////////////////////////
+            ////    Local Dev Code -- Delete Later    ////
+            //////////////////////////////////////////////
+            $this->hostname = "localhost";
+            $this->username = "root"
+            $this->password = ""
+            $this->database = "cryptoadvantage"
+        }
+
+        public function connect(){          
+            try {
+                $this->conn = new PDO("mysql:host=" . $this->hostname . ";dbname=" . $this->database, $this->username, $this->password);
+
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $this->conn;
+            }
+            catch(PDOException $e)
+            {
+                echo "Connection failed: " . $e->getMessage();
+            }            
+        }
+    }
