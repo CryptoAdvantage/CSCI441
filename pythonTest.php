16dda32aca2a62fd314c6d7e8c1669230307157c
Curtis Ruder HP <curtisjruder@gmail.com>
2022-04-07 21:51:55 -0500

    Python integration example

---
+++ pythonTest.php
@@ -0,0 +1,15 @@
+<?php
+$var1 = "TestVariable1";
+$var2 = 900;
+
+echo "<h3>Inside of PHP</h3>";
+echo "Var1 within PHP= " . $var1;
+echo "<br>";
+echo "Var2 within PHP= " . $var2;
+echo "<br><br>";
+
+exec("py ./tradingBot/testing.py $var1 $var2", $output);
+
+foreach($output as $blah){
+    echo $blah;
+}
