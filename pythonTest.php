8bc711251c9764ffe6823bb2758e9a87dcc220ca
Hamad Hamdi <Hammock42@protonmail.com>
2022-04-26 10:47:50 +0200

    testing python test

--- pythonTest.php
+++ pythonTest.php
@@ -1,14 +1,26 @@
 <?php
-$var1 = "TestVariable1";
-$var2 = 900;
+$botName = "TestBot";
+$exchange = "binanceus";
+$token1 = "BTC";
+$token2 = "USD";
+$interval = "1m";
+$strategy = "BB";
 
 echo "<h3>Inside of PHP</h3>";
-echo "Var1 within PHP= " . $var1;
+echo "Bot Name within PHP= " . $botName;
 echo "<br>";
-echo "Var2 within PHP= " . $var2;
+echo "Exchange within PHP= " . $exchange;
+echo "<br>";
+echo "Token 1 within PHP= " . $token1;
+echo "<br>";
+echo "Token 2 within PHP= " . $token2;
+echo "<br>";
+echo "Interval within PHP= " . $interval;
+echo "<br>";
+echo "Strategy within PHP= " . $strategy;
 echo "<br><br>";
 
-exec("python ./tradingBot/pythonTest.py $var1 $var2", $output);
+exec("python ./tradingBot/mainTest.py $botName $exchange $token1 $token2 $interval $strategy", $output);
 
 foreach($output as $blah){
     echo $blah;
--- tradingBot/mainTest.py
+++ tradingBot/mainTest.py
@@ -1,44 +1,74 @@
-import binanceApiFunctions as baf
-import TradingBot as tb
-import time
-import testFunctions as test
-import os
+try:
+    import sys
+except:
+    print("Error importing sys.<br>")
+try:
+    import binanceApiFunctions as baf
+except:
+    print("Error importing binanceApiFunctions.<br>")
+try:
+    import TradingBot as tb
+except:
+    print("Error importing TradingBot.<br>")
+try:
+    import testFunctions as test
+except:
+    print("Error importing testFunctions.<br>")
+try:
+    import os
+except:
+    print("Error importing os.<br>")
 
-baf.initialise(os.environ.get('API_KEY'), os.environ.get('SECRET_KEY'))
+try:
+    baf.initialise(os.environ.get('API_KEY'), os.environ.get('SECRET_KEY'))
+except:
+    print("Error initializing keys.<br>")
 
-# starting data
-token1 = 'BTC'
-token2 = 'USD'
-tradePair = token1+token2
-interval = "1h"
-amount = 100
-tradeFee = 0.99925  # fee per trade multiplier
+try:
+    botName = sys.argv[1]
+    exchange = sys.argv[2]
+    token1 = sys.argv[3].upper()
+    token2 = sys.argv[4].upper()
+    tradePair = token1+token2
+    interval = sys.argv[5]
+    strategy = sys.argv[6]
+    amount = 100
+    tradeFee = 0.99925  # fee per trade multiplier
+except:
+    print("Error storing variables. <br>")
+
+try:
+    print("<h3>Inside of Python Main Test</h3>")
+    print("Bot Name: ", botName, "<br>")
+    print("Exchange: ", exchange, "<br>")
+    print("Token 1: ", token1, "<br>")
+    print("Token 2: ", token2, "<br>")
+    print("Trading Pair: ", token1+token2, "<br>")
+    print("Interval: ", interval, "<br>")
+    print("Trading Strategy: ", strategy, "<br>")
+except:
+    print("Error printing variables. <br>")
 
 # tests connections and structure
 def testRun():
     """Testing the connections and structure"""
-    print("\n\nTesting the Binance API connections:\nTesting . . .\n")
+    print("<br><br>Testing the Binance API connections:<br>")
     test.testConn()
-    time.sleep(5)
-    print("\n\nTesting the Binance API Order Requests:\nTesting . . .\n")
+    print("<br><br>Testing the Binance API Order Requests:<br>")
     test.testOrder(tradePair)
-    time.sleep(5)
-    print("\n\nTesting the Binance API get Kline Request:\nTesting . . .\n")
+    print("<br><br>Testing the Binance API get Kline Request:<br>")
     test.testGetKlines(tradePair, interval)
-    time.sleep(5)
-    print("\n\nTesting the converting Kline data into Pandas DataFrame:\nTesting . . .\n")
+    print("<br><br>Testing the converting Kline data into Pandas DataFrame:<br>")
     test.testGetData(tradePair, interval)
-    time.sleep(5)
 
     # Testing the Trading Bot class
-    print("\n\nTesting the Trading Bot structure:\nTesting . . .\n")
+    print("<br><br>Testing the Trading Bot structure:<br>")
     testBot = tb.TradingBot(amount, token1, token2, tradeFee, interval)
     testBot.test()
-    time.sleep(10)
 
     # Uncomment to test running loop
-    print("\n\nTesting the Trading Bot loop structure:\nTesting . . .\n")
-    test.testBotLoop(token1, token2, interval)
+    #print("<br><br>Testing the Trading Bot loop structure:<br>")
+    #test.testBotLoop(token1, token2, interval)
     
 if __name__ == "__main__":
     testRun()
