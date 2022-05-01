f86b9100550a8d0b1e3aa9aacd71a7e62a50fbf9
Curtis Ruder HP <curtisjruder@gmail.com>
2022-05-01 11:51:50 -0500

    Modified the about and FAQ pages

--- about.php
+++ about.php
@@ -7,9 +7,39 @@
 
 <body>
     <main>
-        <p>Crypto Currency is a growing source of income for a lot of people. There has been different kinds of crypto currency that have been introduced into the market. 
-            You are now able to buy and sell cryptocurrency on different exchanges that have been created. This is an app to help you buy and sell crypto with the use of bots that will be 
-            able to help you figure out when the best time to buy and sell is. You will be able to customize them to your liking and have them running checking the market. </p>
+        <h1 class="fade-transition">Welcome to Crypto Advantage</h1>
+        <p class="fade-transition">
+            In November 2021, the cryptocurrency market capitalization 
+            peaked in excess of $3 trillion dollars.  To put it in 
+            perspective, this market cap rivals that of the highest 
+            value corporation in U.S. history, Apple.  The distinction 
+            is that while the 2021 market capitalization of the two were similar, 
+            the cryptocurrency market cap explosion did in five years what 
+            Apple needed 30 years to do.
+        </p>
+        <p class="fade-transition">
+            Unlike public ownership in companies via the stock market which has 
+            been around for centuries, the cryptocurrency market is relatively new.  
+            However, with its explosive growth and opportunities for gain, 
+            cryptocurrency has been gaining mainstream traction as evidenced by 
+            the many new trading platforms such as Robinhood, exchange traded 
+            funds such as COIN, and recent investment by mainstream institutional 
+            investors.  Although gaining public attention, we believe that the 
+            cryptocurrency market is lacking some of the individualized market 
+            tools seen more widely with stock markets, and we hope to close that gap.
+        </p>
+        <p class="fade-transition">
+            The primary goal of this project is to reach this underserved 
+            crypto market by providing an interactive learning platform 
+            via a cryptocurrency simulated competition and leveraging 
+            technology to provide trading resources via a customizable 
+            trading bot.
+        </p>
+        <p class="fade-transition">
+            Have a look around, explore, and settle in.  We've broken ground on a 
+            project that will give you the power to leverage technology and level the 
+            playing field.  Thanks for joining us in this adventure!
+        </p>
     </main>
 </body>
 
--- faq.php
+++ faq.php
@@ -7,7 +7,43 @@
 
 <body>
     <main>
-        <p>Placeholder for an FAQ</p>
+        <h2 class="fade-transition">How do I get started?</h2>
+        <p class="fade-transition">
+            Route to the log-in page and sign up.  It's free!
+            We promise we won't spam your email or share with others 
+            that will.
+        </p>
+        
+        <h2 class="fade-transition">How do I do I make money?</h2>
+        <p class="fade-transition">
+            The first step is to create a trading bot using the Crypto Advantage 
+            machine learning process.  Once you're happy with your performance, you 
+            only need to tether your bot to the cryptocurrency exchange of your preference.
+        </p>
+
+        <h2 class="fade-transition">What crytocurrency exchanges are supported?</h2>
+        <p class="fade-transition">
+            The list of supported exchanges continually evolves.  Once you log in, you 
+            can view the full list.  Check back regularly because we're adding more daily.
+        </p>
+
+        <h2 class="fade-transition">What cryptocurrencies can be traded?</h2>
+        <p class="fade-transition">
+            Cyptocurrencies are exchange specific.  Check with your favorite exchange 
+            on what's offered, and if we're linked to that exchange, you can trade it.  It's
+            as simple as that.
+        </p>
+
+        <h2 class="fade-transition">How can I give back?</h2>
+        <p class="fade-transition">
+            This project is free to you, and we hope you make lots of money from it.  
+            To give back to us, we only ask that you tell your friends and family if you like it.
+        </p>
+
+        <h2 class="fade-transition">Why am I still reading this?!</h2>
+        <p class="fade-transition">
+            We don't know either, but head on over to the log-in page and sign up now!
+        </p>
     </main>
 </body>
 
