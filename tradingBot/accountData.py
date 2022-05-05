c0e377d80c94612f6ab73d22f4bd95e5de2defa9
Hamad Hamdi <Hammock42@protonmail.com>
2022-05-05 11:58:18 +0200

    added file to update accounts in DB

--- tradingBot/accountData.py
+++ tradingBot/accountData.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import mysql.connector
+import json
 
 # testing
 try:
@@ -8,30 +9,54 @@ except:
     print("Error importing binance api functions! <br>")
 
 try:
-    exchange = sys.argv[1]
-except:
-    print("Error storing exchange variable. <br>")
-
-try:
     baf.initialise(os.environ.get('API_KEY'), os.environ.get('SECRET_KEY'))
 except:
     print("Error initializing keys.<br>")
 
-""" try:
-    DATABASE_URL=os.environ.get('JAWSDB_URL')
-    print(DATABASE_URL)
+try:
+    cnx = mysql.connector.connect(
+        user=os.environ.get('USER'),
+        password=os.environ.get('PASS'),
+        host=os.environ.get('HOST'),
+        database=os.environ.get('DATABASE')
+    )
+    mycursor = cnx.cursor()
 except:
-    print("Error connecting to database. <br>") """
+    print("Error connecting to database. <br>")
+
+def storeAccountData():
+    data = baf.getUsrAccountData()
+    accounts = {}
+    for d in data["balances"]:
+        tempAccount = {}
+        tempAccount["free"] = d["free"]
+        tempAccount["locked"] = d["locked"]
+        tempAccount["total"] = str(float(d["free"])+float(d["locked"]))
+        accounts[d["asset"]] = tempAccount
+    accounts = json.dumps(accounts)
+    user_id = 14
+    exch_id = 10
+    done = False
+    while(not done):
+        mycursor.execute(f"SELECT `id` FROM user_account WHERE `user_id`=14 AND `exch_id`=10")
+        for x in mycursor:
+            if (type(x[0] == int)):
+                id = x[0]
+                sqlUpdate = (f"UPDATE user_account SET `docs`='{accounts}' WHERE `id`={id}")
+                mycursor.execute(sqlUpdate)
+                done = True
+                break
+        if (done): break
+        mycursor.reset()
+        sqlInsert = (f"INSERT INTO user_account (`user_id`, `exch_id`, `docs`) VALUES ({user_id}, {exch_id}, '{accounts}')")
+        mycursor.execute(sqlInsert)
+        cnx.commit()
+        break
+
+    mycursor.close()
+    cnx.close()
 
 try:
-    data = baf.getUsrAccountData()
-    print("Token &emsp; Available &emsp; Locked &emsp; Total<br>")
-    for d in data["balances"]:
-        asset = d["asset"]
-        free = d["free"]
-        locked = d["locked"]
-        total = (float(free)+float(locked))
-        total = str(total)
-        print(f"{asset} &emsp; {free} &emsp; {locked} &emsp; {total}<br>")
+    storeAccountData()
 except:
-    print("Error getting account data.")
+    print("Error storing account data.")
