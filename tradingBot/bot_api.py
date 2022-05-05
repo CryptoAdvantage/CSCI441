427d9a194de38fdb2cf1a99718fbc75f119c990b
Hamad Hamdi <Hammock42@protonmail.com>
2022-05-05 09:32:03 +0200

    testing bot_api

--- tradingBot/bot_api.py
+++ tradingBot/bot_api.py
@@ -16,20 +16,55 @@ except:
     print("Error imprting binance api functions! <br>")
 
 try:
+    import models
+except:
+    print("Error importing models")
+
+try:
     botName = sys.argv[1]
     exchange = sys.argv[2]
     token1 = sys.argv[3].upper()
     token2 = sys.argv[4].upper()
     interval = sys.argv[5]
     strategy = sys.argv[6]
-    amount = 100
+    posted = sys.argv[7]
+    email = sys.argv[8]
+    amount = 1000
     tradeFee = 0.99925  # fee per trade multiplier
     tradePair = token1+token2
+    if (strategy == 'bb'):
+        strategy = 'Simple Bollinger Bands'
 except:
     print("Error storing variables. <br>")
 
+""" try:
+    user = 10
+    botName = "Testing1"
+    exchange = "binanceus"
+    token1 = "BTC"
+    token2 = "USD"
+    interval = "1h"
+    strategy = "BB"
+    amount = 1000
+    tradePair = token1+token2
+    tradeFee = 0.99925
+except:
+    print("Error storing variables. <br>") """
+
+""" try:
+    #baf.initialise(os.environ.get('API_KEY'), os.environ.get('SECRET_KEY'))
+    api = "CqQEHo0a1vY6BuoQKhL72B0nGzIjf2w8fKo0iTobJnpfLzaHfwgkZGVFzEb91kTG"
+    sec = "FtX1LVJYGkKnauLBoirxuA13XVvSDug27QuC6bxcNpLdcm0R9RnzeLbAStFwLswH"
+    baf.initialise(api, sec)
+    print(baf.testConnection())
+    t_bot = tb.TradingBot(user, botName, strategy, exchange, amount, token1, token2, tradeFee, interval)
+    models.bb_simple(t_bot)
+except:
+    print("Error running bot.<br>") """
+
 try:
-    print("<h3>Inside of Python</h3>")
+    print("<h3>Trading Bot Settings:</h3>")
+    print("User Email: ", email, "<br>")
     print("Bot Name: ", botName, "<br>")
     print("Exchange: ", exchange, "<br>")
     print("Token 1: ", token1, "<br>")
@@ -37,5 +72,7 @@ try:
     print("Trading Pair: ", token1+token2, "<br>")
     print("Interval: ", interval, "<br>")
     print("Trading Strategy: ", strategy, "<br>")
+    if (posted == "true"):
+        print("Store to DB")
 except:
     print("Error printing variables. <br>")
